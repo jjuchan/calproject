bed30008ecee757810fd685fcaea64a3e7013bb6
jjuchan <yjc7241@naver.com>
2025-08-01 14:42:52 +0900

    ëë덧ã셈 기능 추가

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,7 +1,20 @@
+import java.util.StringTokenizer;
+
 public class Calc {
 
 
-    public static int run(String s) {
-        return 2;
+    public static int run(String expression) {
+
+        StringTokenizer st = new StringTokenizer(expression);
+
+        int a = Integer.parseInt(st.nextToken());
+        String b = st.nextToken();
+        int c = Integer.parseInt(st.nextToken());
+
+        if (b.equals("+")) {
+            return a + c;
+        }
+
+        return -1;
     }
 }
